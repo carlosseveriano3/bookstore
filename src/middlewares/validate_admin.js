65d8645924301b0eca8e8677f4e8938aa44b6783
Matheus Marques Lima <matheusm25@hotmail.com>
2024-04-18 19:22:44 -0300

    chore: validate token

--- src/middlewares/validate_admin.js
+++ src/middlewares/validate_admin.js
@@ -1,9 +1,13 @@
 import jwt from 'jsonwebtoken';
 
 export function validateJwt(request, reply, done) {
-    const { authentication } = request.headers
+    const { authorization } = request.headers
     
-    const token = authentication.replace("Bearer ", "")
+    const token = authorization?.replace("Bearer ", "")
+
+    if (!token) {
+        return reply.status(401).send({ message: 'token not found' })
+    }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_KEY);
