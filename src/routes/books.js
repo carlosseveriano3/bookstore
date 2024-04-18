c08d9088f9ac229ce26a52477e9d380f9e333aaa
Matheus Marques Lima <matheusm25@hotmail.com>
2024-04-18 19:22:23 -0300

    chore: apply validation

--- src/routes/books.js
+++ src/routes/books.js
@@ -4,10 +4,10 @@ export async function BooksRouter(app) {
     const booksController = new BooksController();
 
     app.post('/books', async (request, reply) => {
-        return await booksController.createBook(request, reply); 
+        return await booksController.createBook(request, reply);
     });
 
-    app.get('/books', async (request, reply) => {
+    app.get('/books', { preHandler: [app.verifyJwt] }, async (request, reply) => {
         return await booksController.listBooks(request, reply);
     });
 }
